208ffe6eaecf449151c12faf19e069d6db760dbc
Ben Whittle <b.j.whittle2@newcastle.ac.uk>
2021-02-03 16:27:24 +0000

    in case I need it again

---
+++ create_rna_availability.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+rna_pat_visits <- read.csv("~/OneDrive - Newcastle University/PhD/PPMI/rna_pat_visits.csv", header = TRUE)
+
+bl <- read.csv("~/OneDrive - Newcastle University/PhD/PPMI/ppmi_bl_samples_filt.csv", header = T)
+
+bl <- bl %>% 
+  select(PATNO, status_clin, status_imag, clin_imag_agree, sex, age_bl, ppmi_enroll)
+
+rnaseq_availability <- merge(bl, rna_pat_visits, by = "PATNO")
+
+write_csv(rnaseq_availability, "rnaseq_availability.csv")
